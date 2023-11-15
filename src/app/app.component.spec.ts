a79446a53048a081eeb4bdc947c4e03ccdc00fba
Álvaro Fuentenebro <alvarofuentenebrogomez@gmail.com>
2023-11-15 13:18:29 +0100

    feat: created header and routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('investor-tracker-frontend');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, investor-tracker-frontend');
-  });
 });
---
+++ src/app/pages/investors/investors.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investors',
+  templateUrl: './investors.component.html',
+  styleUrl: './investors.component.scss',
+  standalone: true,
+})
+export class InvestorsComponent {}
